ea6ebdd4f6fb2a54160a3c7d90bb9a872f8bb4ac
Dan Breczinski <dan.breczinski@expandtheroom.com>
2019-04-09 17:24:54 -0400

    add about

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="h-screen w-full px-4 py-24" style="padding-top: 4.5rem;">
-    <p>Lorem ipsum dolor sit amet, vel at dolorum quaestio. Ex qui aliquam moderatius quaerendum, ea eius doming civibus nec. An facete utroque denique mei, est an eius singulis scriptorem. No mel alienum antiopam intellegebat, sit eu ridens debitis convenire. Mel ad amet omnes mandamus, choro fabellas perfecto qui ex, qui veniam oblique ut.</p>
-
-    <p>Ea insolens interesset reprehendunt vix, eum in dicunt vidisse, usu labore expetendis ut. Natum labitur scribentur ex his, cum ei consulatu reprehendunt mediocritatem. Vis eu invenire reprimique concludaturque, alii debet vix ne. Idque gloriatur ut pri, in qui minim solet maluisset. Id has ludus zril consetetur. Omittam appareat persecuti ad mei, quo te brute populo persequeris.</p>
-
-    <p>Ius id suas reformidans, vel reque hendrerit ne. Ex his quaerendum referrentur. Eum an constituto mediocritatem, vel hinc doming eu. Dicam iisque assentior ius ut, deleniti nominati reprehendunt te est.</p>
+  <div class="h-screen w-full px-6 py-24" style="padding-top: 4.5rem;">
+    <p>ArtNul is contemporary, square format, and generative.</p>
+    <p>ArtNul is code as a first class medium.</p>
+    <p>ArtNul directs randomness and curates.</p>
   </div>
 </template>
 
